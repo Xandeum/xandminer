907175c65d0ac4c46d484c8d7da90717f77f56f3
Kaveen Laksitha <kaveen_lak@bitoku.io>
2023-11-11 02:11:34 +0530

    added option to select amount to dedicate

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -4,18 +4,24 @@ import Box from '@mui/material/Box';
 import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
 import prettyBytes from 'pretty-bytes';
 import { getDriveInfo } from '../../services/getDriveInfo';
+import Slider from '@mui/material/Slider';
+import { Input } from '@mui/base';
+import { TextField } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import { AddBox, IndeterminateCheckBox } from '@mui/icons-material';
 
 export const HomeView: FC = ({ }) => {
 
   const [driveInfo, setDriveInfo] = React.useState<Array<any>>([]);
   const [isFetching, setIsFetching] = React.useState<boolean>(false);
-  const [progress, setProgress] = React.useState(300);
+  const [dedicatingAmnt, setDedicatingAmnt] = React.useState([{ disk: 0, amount: 0 }]);
 
   React.useEffect(() => {
     setIsFetching(true);
     getDriveInfo().then((response) => {
       if (response.ok) {
         setDriveInfo(response.data);
+        setDedicatedInitialAmnt(response.data)
         setIsFetching(false);
       }
     }).catch((error) => {
@@ -24,8 +30,28 @@ export const HomeView: FC = ({ }) => {
     })
   }, []);
 
+  const setDedicatedInitialAmnt = (data: Array<any>) => {
+    let drives = [];
+    data.forEach((drive, index) => {
+      drives.push({ disk: index, amount: 0 });
+    });
+    setDedicatingAmnt(drives);
+    return;
+  }
+
   // Function to normalise the values (MIN / MAX could be integrated)
   const normalise = (value, MIN, MAX) => ((value - MIN) * 100) / (MAX - MIN);
+
+  //check the input value is greater than max value
+  const forceToMax = (index, max, input) => {
+    if (input > max) {
+      setDedicatingAmnt(prevState => {
+        const updatedArray = [...prevState];
+        updatedArray[index] = { disk: index, amount: max };
+        return updatedArray;
+      });
+    }
+  }
 
   return (
     <div className="flex flex-col items-center md:items-start w-full md:px-16 p-8">
@@ -33,7 +59,7 @@ export const HomeView: FC = ({ }) => {
         <p>Drive Information</p>
       </h4>
       <div className='border-b border-[#4a4a4a] mb-8 mt-2 w-full' />
-      <div className="flex flex-row items-center justify-start flex-wrap gap-8 w-full px-4">
+      <div className="flex flex-row items-start justify-start flex-wrap gap-8 w-full px-4 md:min-h-[45vh]">
         {
           driveInfo.length > 0 ?
             driveInfo.map((drive, index) => {
@@ -56,8 +82,93 @@ export const HomeView: FC = ({ }) => {
                           </Box>
                         </Box>
                       </Box>
-                      {/* </div> */}
+                      <div className='border-b border-[#4a4a4a] my-4 w-full' />
+                      <p>Dedicate space</p>
 
+                      <Box sx={{ width: '100%' }}>
+                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                          <Box sx={{ width: '100%', mx: '0.35rem' }}>
+                            <Slider
+                              size="medium"
+                              defaultValue={0}
+                              min={1000000000}
+                              max={drive?.capacity}
+                              aria-label="Small"
+                              valueLabelDisplay="off"
+                              value={dedicatingAmnt[index]?.amount}
+                              onChange={(event, value) => {
+                                setDedicatingAmnt(prevState => {
+                                  const updatedArray = [...prevState];
+                                  updatedArray[index] = { disk: index, amount: value as number };
+                                  return updatedArray;
+                                });
+                              }}
+
+
+                            />
+                          </Box>
+                          {/* <span>{(prettyBytes(dedicatingAmnt[index]?.amount)).split(" ")[1]}</span> */}
+
+
+                        </Box>
+                      </Box>
+
+                      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', width: '100%', }}>
+                        <IconButton
+                          aria-label="delete"
+                          color='info'
+                          onClick={() => {
+                            setDedicatingAmnt(prevState => {
+                              const updatedArray = [...prevState];
+                              updatedArray[index] = { disk: index, amount: updatedArray[index].amount - 10000000000 };
+                              return updatedArray;
+                            });
+                          }}
+                          disabled={dedicatingAmnt[index]?.amount - 10000000000 <= 0}
+                        >
+                          <IndeterminateCheckBox />
+                        </IconButton>
+                        <TextField
+                          id="outlined-basic"
+                          variant='outlined'
+                          value={(dedicatingAmnt[index]?.amount / 1000000000).toFixed(0)}
+                          size="small"
+                          onChange={(e) => {
+                            setDedicatingAmnt(prevState => {
+                              const updatedArray = [...prevState];
+                              updatedArray[index] = { disk: index, amount: Math.abs(isNaN(parseFloat(e.target.value)) ? 0 : parseFloat(e.target.value) * 1000000000) };
+                              return updatedArray;
+                            });
+                          }}
+
+                          onBlur={forceToMax(index, drive?.capacity, dedicatingAmnt[index]?.amount)}
+                          InputProps={{
+                            inputProps: { min: 1000000000 },
+                            endAdornment: (
+                              <div className='flex flex-row items-center ml-1  text-white'>
+                                <span className='pb-1'>{(prettyBytes(dedicatingAmnt[index]?.amount || 0))?.split(" ")[1] || null}</span>
+                              </div>
+                            ),
+                          }}
+                          className='text-white bg-black w-1/2'
+                          sx={{
+                            input: { color: 'white', textAlign: 'right', width: '50%' },
+                          }}
+                        />
+                        <IconButton
+                          aria-label="delete"
+                          color='info'
+                          onClick={() => {
+                            setDedicatingAmnt(prevState => {
+                              const updatedArray = [...prevState];
+                              updatedArray[index] = { disk: index, amount: updatedArray[index].amount + 10000000000 };
+                              return updatedArray;
+                            });
+                          }}
+                        >
+                          <AddBox />
+                        </IconButton>
+                      </Box>
                     </div>
                   </div>
                 </div>
@@ -69,6 +180,8 @@ export const HomeView: FC = ({ }) => {
             </div>
         }
       </div>
+      {/* <div className='border-b border-[#4a4a4a] mb-8 mt-2 w-full' /> */}
+
     </div>
   );
 };
