bdb85b5782aeb466cb86462fb124bfca9360221f
KaveenLK <kmklaksitha@gmail.com>
2025-03-18 06:07:29 +0100

    updated devnet program id

--- src/CONSTS.ts
+++ src/CONSTS.ts
@@ -6,5 +6,5 @@ export const XANDMint = new PublicKey("XANDuUoVoUqniKkpcKhrxmvYJybpJvUxJLr21Gaj3
 export const FEE_DEPOSIT_ACC = new PublicKey("82m8SFM5ggHrCQYbD8nC8HxWRx4YHRR7RQAdeC8RNtyX");
 export const REFUNDABLE_DEPOSIT_ACC = new PublicKey("FaWhmBgWevoJGcJyrx7CHaf6WNduMjEBb6WQEccSVp6Z");
 
-export const DEVNET_PROGRAM = new PublicKey("2YCmooMUuhAZRcxKVA62zc9A2NiWfqbCnJbm53UT4aic");
+export const DEVNET_PROGRAM = new PublicKey("6Bzz3KPvzQruqBg2vtsvkuitd6Qb4iCcr5DViifCwLsL");
 export const PNODE_PROGRAM = new PublicKey("3hMZVwdgRHYSyqkdK3Y8MdZzNwLkjzXod1XrKcniXw56");
