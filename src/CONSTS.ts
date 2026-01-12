d8ac6875c64dc24e5bff90084111dffdee055d04
Kaveen Laksitha <kmklaksitha@gmail.com>
2026-01-12 16:46:43 +0530

    bumped up the version

--- src/CONSTS.ts
+++ src/CONSTS.ts
@@ -20,11 +20,13 @@ export const KEYPAIR_PATH = "../xandminerd/keypairs/pnode-keypair.json";
 
 export const PROGRAM = new PublicKey("CZ9bXL6D4uiLXGsSk5s8KAgTFEVp3gdpxPxTCrgm3VoL"); // mainnet
 
-export const VERSION_NO = "v0.8.0";
-export const VERSION_NAME = "Reinheim";
+export const VERSION_NO = "v1.2.0";
+export const VERSION_NAME = "Bonn";
 
 export const getVersionName = (versionNo: string) => {
-    if (versionNo.startsWith("v0.8.")) {
+    if (versionNo.startsWith("v1.2.")) {
+        return VERSION_NAME;
+    } else if (versionNo.startsWith("v0.8.")) {
         return "Reinheim";
     } else if (versionNo.startsWith("v0.7.")) {
         return "Heidelberg";
@@ -40,3 +42,4 @@ export const getVersionName = (versionNo: string) => {
         return " "
     }
 }
+
