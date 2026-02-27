3e6dfe120118cf93b6168e0f17cbe753fe2a8b9c
Kaveen Laksitha <kmklaksitha@gmail.com>
2026-02-27 11:53:19 +0530

    updated the responsiveness

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -58,9 +58,8 @@ export const AppBar: React.FC = () => {
         {/* Navbar end */}
 
         <div className="navbar-end pr-2 md:pr-10">
-          <div className="inline-flex items-center justify-center gap-6">
-            {/* <Link href={"/store"} className='text-white hover:text-[#fda31b]'>Store</Link> */}
-            <div className='flex items-center gap-4'>
+          <div className="inline-flex items-center justify-center">
+            <div className='flex md:gap-4 md:flex-row flex-col items-end md:pb-0 py-4 gap-1 md:items-baseline'>
               <button
                 className="btn btn-ghost btn-sm rounded-btn text-base normal-case"
                 onClick={() => setShowRpcModal(true)}
