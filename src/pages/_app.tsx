bf95c3ff7df5d244a1f4b79aa9c23a8e19761cef
Kaveen Laksitha <kmklaksitha@gmail.com>
2026-03-20 21:37:12 +0530

    updated dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,8 @@ import { AppBar } from '../components/AppBar';
 import { ContentContainer } from '../components/ContentContainer';
 import { Footer } from '../components/Footer';
 import Notifications from '../components/Notification'
-import React from 'react';
-require('@solana/wallet-adapter-react-ui/styles.css');
-require('../styles/globals.css');
+import '@solana/wallet-adapter-react-ui/styles.css';
+import '../styles/globals.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
